0341fc9a95c35858dbe976696edfbb4cfaa10356
Luiza <luizambastos@hotmail.com>
2020-02-11 17:02:25 -0300

    adiciona dados limpos e primeiro script

---
+++ R/script.R
@@ -0,0 +1,7 @@
+#script para ler a tabela limpa
+#os dados originais estao em um xlsx
+
+help(read.table)
+read.csv2("./data/ex04(formatado).csv")
+#read.csv2 para linguas latinas (separador ;); sep + ";", dec = "," para substituir o separador por ponto
+dir("data")
