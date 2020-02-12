15cfc9d1e530f0bdacd4e44d327c7f3badb948da
Luiza <luizambastos@hotmail.com>
2020-02-12 10:03:00 -0300

    renamed script

--- R/Aula2.R
+++ R/Aula2.R
@@ -1,7 +1,11 @@
+#Projeto de análise de dados- Aula 2
 #script para ler a tabela limpa
 #os dados originais estao em um xlsx
 
+
 help(read.table)
-read.csv2("./data/ex04(formatado).csv")
+read.csv2("./data/ex04_formatado.csv")
+
 #read.csv2 para linguas latinas (separador ;); sep + ";", dec = "," para substituir o separador por ponto
 dir("data")
+
